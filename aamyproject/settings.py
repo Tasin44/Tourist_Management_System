ff39135e8dd885e40176b60588ffbb532afd6e15
Tasin44 <tasinmahmud44@gmail.com>
2026-09-12 09:48:58 +0600

    migrations done

--- aamyproject/settings.py
+++ aamyproject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default-insecure-key-if-not-found')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.10.29.34', '127.0.0.1', 'localhost']
 
 
 # Application definition
